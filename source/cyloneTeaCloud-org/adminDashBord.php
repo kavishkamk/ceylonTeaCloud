4c1ab20e323d3f4ed67e12186d23f932596ab3aa
kavishkamk <kavishkamadhushan727@gmail.com>
2021-11-05 21:00:46 +0530

    added admin dash board UI

---
+++ source/cyloneTeaCloud-org/adminDashBord.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title> 
+        <link rel="stylesheet" type="text/css" href="../css/adminDashBord.css">
+    </head>
+    <body>
+        <div class="container">
+            <nav>
+                <div class="top-bar">
+                    <form>
+                        <button class="log-out-btn" formaction="">Log Out</button>
+                    </form>
+                </div>
+            </nav>
+            <main>
+                <div class="com-abo" style="grid-column:1 / 2; grid-row: 1 / 3;">
+                    <h1>Cylone Tea Cloud</h1>
+                    <p>Factory Name: </p>
+                    <p>Address:</p>
+                    <p>Tele:</p>
+                    <h1>Factory Owner Details</h1>
+                    <p>Name:</p>
+                    <p>Tele</p>
+                </div>
+                <div class="gro-abo" style="grid-column:2 / 3; grid-row: 1 / 2;">
+                    <p>Total Growers</p>
+                    <p>15</p>
+                </div>
+                <div style="grid-column:2 / 3; grid-row: 2 / 3;">
+                    <p>Total Weekly report - 29</p>
+                    <p>Total monthly report - 15</p>
+                </div>
+            </main>
+
+            <div id="sidebar">
+                <h1>
+                    <label for="">
+                        <span class="las la-bars"></span>
+                    </label>
+                    <img src=".."></img>
+                </h1>
+                <div>
+                    <div class="dash-nav">
+                        <span>Factory Name</span>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Dashboard</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Grower List</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Pending Request</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Comfirmed Request</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Weekly Report</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Monthely Report</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>Profile Setting</span>
+                        </a>
+                    </div>
+                    <br>
+                    <div class="dash-nav">
+                        <a href="">
+                            <span>About</span>
+                        </a>
+                    </div>
+                    <br>
+                </div>
+                <br>
+                <br>
+            </div>
+        </div>
+    </body>
+</html>
