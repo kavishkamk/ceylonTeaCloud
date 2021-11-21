a9d744859c90eaea1acd0ec8e37808f962eb567e
Vishvi98 <vishvis@sltc.ac.lk>
2021-11-21 22:27:19 +0530

    refactor(Profile Pic): Renaming and Refactoring

--- source/grower-ui/profilePic.php
+++ source/grower-ui/profilePic.php
@@ -4,38 +4,32 @@
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>PROFILE PICTURE SETTINGS</title>
+    <title>Change Profile Picture</title>
 
-    <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
     />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../css/propic.css"/>
+    <link rel="stylesheet" href="../css/profilePic.css"/>
 
 </head>
 
 <body>
 <div class="main-container">
     <div class="container">
-        <h2 class="login-title">Profile Picture Settings</h2>
+        <h2 class="login-title">Change Profile Picture</h2>
         <div class="form-container">
             <form action="profilePic.php" method="post">
-
-                <!-- profile picture -->
                 <div class="form-group text-field-container">
                     <form action="profilePic.php" method="post">
                         <div class="image">
                             <img src="../images/vishvi.jpeg" alt="Avatar" style="width: 200px"/>
-                            <input type="image" placeholder="image" value=""/>
+<!--                            <input type="file" placeholder="image" value=""/>-->
                         </div>
                     </form>
                 </div>
-
-                <a href="#" class="btn btn-primary btn-lg btn-block login-button">Edit</a>
-                <br>
-
+                <button href="#" class="btn btn-primary btn-lg btn-block login-button">Upload Profile Picture</button>
             </form>
         </div>
         <div class="login-button-container">
