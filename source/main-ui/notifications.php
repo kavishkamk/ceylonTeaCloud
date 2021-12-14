785b15c62b670af3340175358219266000291dd2
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2021-12-14 11:13:53 +0530

    notifications page created

--- source/main-ui/notifications.php
+++ source/main-ui/notifications.php
@@ -10,11 +10,24 @@
         />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="../css/main-menu.css"/>
+        <link rel="stylesheet" href="../css/notifications.css"/>
         <title>Notifications</title>
     </head>
 
     <body>
+        <div class="main-container">
+            <div class="container">
+                <h1 class="home-title">Notifications</h1>
 
+                <div class="grower-home-options-container">
+                    <!--
+                    <div class="notif-item">
+                        <div class="date">2021-01-15</div>
+                        <div class="msg">December Monthly report added</div>
+                    </div>-->
+
+                </div>
+            </div>
+        </div>
     </body>
 </html>
