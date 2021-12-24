ef49e98fecf8b7f57c183d040d8040ffb46854d6
kavishkamk <kavishkamadhushan727@gmail.com>
2021-12-24 13:46:09 +0530

    css link changed

--- source/cyloneTeaCloud-org/adminDashBord.php
+++ source/cyloneTeaCloud-org/adminDashBord.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Admin Dashboard</title> 
-        <link rel="stylesheet" type="text/css" href="../css/adminDashBord.css"/>
+        <link rel="stylesheet" href="../css/adminDashBord.css"/>
     </head>
     <body>
         <div class="container">
